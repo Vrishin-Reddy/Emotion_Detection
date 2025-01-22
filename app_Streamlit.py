4c1bc76c5ac8a06156f6f334ec4dd68ca51549f4
Vrishin-Reddy <94974404+Vrishin-Reddy@users.noreply.github.com>
2025-01-21 23:48:31 -0500

    Update app_Streamlit.py

--- app_Streamlit.py
+++ app_Streamlit.py
@@ -16,7 +16,9 @@ st.set_page_config(
 )
 
 # Paths to the saved models and results
-save_dir = os.path.join(os.getcwd(), "Emotion_Detection", "model_saves")
+# Set the absolute path for the model directory
+save_dir = "/mount/src/emotion_detection/Emotion_Detection/model_saves/"
+
 
 results_file = os.path.join(save_dir, "results.csv")
 vectorizer_file = os.path.join(save_dir, "tfidf_vect.pk")
